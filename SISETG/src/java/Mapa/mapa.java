f23070e93940a1f3b7b50f2291564b228ffbcaa1
R <rafces@gmail.com>
2012-09-09 11:18:30 -0600

    R
    Intentos con el mapa

--- SISETG/src/java/Mapa/mapa.java
+++ SISETG/src/java/Mapa/mapa.java
@@ -4,35 +4,20 @@
  */
 package mapa;
 
-import dominio.Canton;
+
 import dominio.Departamento;
 import dominio.Municipio;
-import dominio.Ubicacion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.sql.DataSource;
 import javax.sql.rowset.CachedRowSet;
-import org.primefaces.event.map.PointSelectEvent;
-import org.primefaces.event.map.StateChangeEvent;
-import org.primefaces.model.map.DefaultMapModel;
-import org.primefaces.model.map.LatLng;
-import org.primefaces.model.map.LatLngBounds;
-import org.primefaces.model.map.MapModel;
-import org.primefaces.model.map.Marker;
-import org.primefaces.model.map.Polygon;
-import org.primefaces.model.map.Polyline;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 
 /**
  *
@@ -44,8 +29,8 @@ public class mapa {
     
     private String CodDepartamento;
     private String CodMunicipio;
-    private double Longitud;
-    private double Latitud;
+    private Double Longitud;
+    private Double Latitud;
     
     @Resource(name = "jdbc/sise")
     DataSource dataSource;
@@ -62,19 +47,19 @@ public class mapa {
         this.CodDepartamento = CodDepartamento;
     }
 
-    public double getLongitud() {
+    public Double getLongitud() {
         return Longitud;
     }
 
-    public void setLongitud(float Longitud) {
+    public void setLongitud(Double Longitud) {
         this.Longitud = Longitud;
     }
     
-        public double getLatitud() {
+        public Double getLatitud() {
         return Latitud;
     }
 
-    public void setLatitud(float Latitud) {
+    public void setLatitud(Double Latitud) {
         this.Latitud = Latitud;
     }
     
@@ -96,7 +81,7 @@ public class mapa {
 
      
     /****** Para sacar las coordenadas*/
-    public double obtenerLat(String CodDepartamento) throws SQLException{
+    public Double obtenerLat(String Seleccion) throws SQLException{
         if (dataSource == null) {
             throw new SQLException("No se pudo tener acceso a la fuente de datos");
         }
@@ -107,12 +92,10 @@ public class mapa {
         }
         try {
             PreparedStatement obtenerLat = connection.prepareStatement(
-                    "SELECT LATITUD FROM UBICACION WHERE IDUBIC_PADRE = '" + CodMunicipio+"'");
+                    "SELECT LATITUD FROM UBICACION WHERE IDUBIC = '" + Seleccion+"'");
             ResultSet lati = obtenerLat.executeQuery();
             Latitud = lati.getDouble("LATITUD");
-            FacesContext context = FacesContext.getCurrentInstance();  
-          
-        context.addMessage(null, new FacesMessage("Successful", "Hello " + Latitud));  
+            
             return Latitud;
         } finally {
             connection.close();
@@ -120,7 +103,7 @@ public class mapa {
         //return 0;
 }
     
-    public double obtenerLng(String CodDepartamento) throws SQLException{
+    public Double obtenerLng(String Seleccion) throws SQLException{
         if (dataSource == null) {
             throw new SQLException("No se pudo tener acceso a la fuente de datos");
         }
@@ -131,7 +114,7 @@ public class mapa {
         }
         try {
             PreparedStatement obtenerLng = connection.prepareStatement(
-                    "SELECT LONGITUD FROM UBICACION WHERE IDUBIC_PADRE = '" + CodMunicipio+"'");
+                    "SELECT LONGITUD FROM UBICACION WHERE IDUBIC = '" + Seleccion+"'");
             ResultSet longi = obtenerLng.executeQuery();
             Longitud = longi.getDouble("LONGITUD");
             
@@ -189,74 +172,10 @@ public class mapa {
                 resultados.add(new Municipio(rowSet.getString("IDUBIC"),
                         rowSet.getString("NOMBUBIC")));
             }
-            obtenerLat(CodDepartamento);
             return resultados;
         } finally {
             connection.close();
         }
     }
     
-        public List<Canton> getCantones() throws SQLException {
-        List<Canton> resultados = new ArrayList<Canton>();
-        if (dataSource == null) {
-            throw new SQLException("No se pudo tener acceso a la fuente de datos");
-        }
-        Connection connection = dataSource.getConnection();
-        if (connection == null) {
-            throw new SQLException("No se pudo conectar a la fuente de datos");
-        }
-        try {
-            String query = "SELECT IDUBIC, NOMBUBIC FROM UBICACION WHERE IDUBIC_PADRE = '" + CodMunicipio + "' order by NOMBUBIC";
-            PreparedStatement getUbicacion = connection.prepareStatement(query);
-            CachedRowSet rowSet = new com.sun.rowset.CachedRowSetImpl();
-            rowSet.populate(getUbicacion.executeQuery());
-            while (rowSet.next()) {
-                resultados.add(new Canton(rowSet.getString("IDUBIC"),
-                        rowSet.getString("NOMBUBIC")));
-            }
-            return resultados;
-        } finally {
-            connection.close();
-        }
-    }
-    
-
- 
-    public Map<String, String> getListarMunicipios(String CodigoDep) throws SQLException{
-        Map<String, String> mapa = new HashMap();
-        ArrayList<Ubicacion> lista = new ArrayList<Ubicacion>();
-        
-        if (dataSource == null) {
-            throw new SQLException("No se pudo tener acceso a la fuente de datos");
-        }
-        Connection connection = dataSource.getConnection();
-
-        if (connection == null) {
-            throw new SQLException("No se pudo conectar a la fuente de datos");
-        }
-         try {
-            String query = "SELECT IDUBIC, NOMBUBIC FROM UBICACION WHERE IDUBIC_PADRE = '" + CodigoDep + "' order by NOMBUBIC";
-            PreparedStatement getUbicacion = connection.prepareStatement(query);
-            //JOptionPane.showMessageDialog(null,query);
-            CachedRowSet rowSet = new com.sun.rowset.CachedRowSetImpl();
-            rowSet.populate(getUbicacion.executeQuery());
-            while (rowSet.next()) {
-                lista.add(new Ubicacion(rowSet.getString("IDUBIC"),
-                        rowSet.getString("NOMBUBIC")));
-            }
-            
-            //JOptionPane.showMessageDialog(null,resultados.size());
-                for (Ubicacion object : lista){
-                    //Map map = (Map) rowSet;
-                   // mapa.put(map.get("NOMBUBIC").toString(), map.get("IDUBIC").toString());
-                   Map map = (Map) object;
-                   mapa.put(map.get("nombreUbicacion").toString(), map.get("idUbicacion").toString());
-                }
-                return mapa;
-        } finally {
-            connection.close();
-        }
-    }
-
-  
 }
