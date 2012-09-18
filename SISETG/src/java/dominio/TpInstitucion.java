97054f0184476cb97d118a77f1513cfa63c8e5fa
R <rafces@gmail.com>
2012-09-18 15:39:17 -0600

    R
    Avances Registro Institucion

--- SISETG/src/java/dominio/TpInstitucion.java
+++ SISETG/src/java/dominio/TpInstitucion.java
@@ -4,16 +4,27 @@
  */
 package dominio;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.sql.DataSource;
+
 /**
  *
  * @author Administrador
  */
-
+@ManagedBean
+@RequestScoped
 public class TpInstitucion {
 
     private int IdTpInstitucion;
     private String NombreTpInstitucion;
     private String DescTpInstitucion;
+    @Resource(name = "jdbc/sise")
+    DataSource dataSource;
     
     public TpInstitucion(int IdTpInstitucion, String NombreTpInstitucion, String DescTpInstitucion){
         this.IdTpInstitucion = IdTpInstitucion;
@@ -41,5 +52,29 @@ public class TpInstitucion {
 
     public void setDescTpInstitucion(String DescTpInstitucion) {
         this.DescTpInstitucion = DescTpInstitucion;
+    }
+    public String guardar() throws SQLException{
+        if (dataSource == null) {
+            throw new SQLException("No se pudo tener acceso a la fuente de datos");
+        }
+
+        Connection connection = dataSource.getConnection();
+
+        if (connection == null) {
+            throw new SQLException("No se pudo conectar a la fuente de datos");
+        }
+        CallableStatement cs;
+        try{
+            String sql="{ call sp_addTpInst(?,?)}";
+            cs = connection.prepareCall(sql);
+                cs.setString(1, getNombreTpInstitucion());
+                cs.setString(2, getDescTpInstitucion());
+                
+                cs.execute();
+        }
+        finally{
+            connection.close();
+        }
+        return "index";
     }
 }
