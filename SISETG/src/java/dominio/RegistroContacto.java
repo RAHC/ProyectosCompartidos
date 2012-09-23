5c9d0300f38ba67824fb75e0cbe257b58ad62cc7
Claudia <clausorto88@gmail.com>
2012-09-23 08:28:51 +0200

    Cambios SP

--- SISETG/src/java/dominio/RegistroContacto.java
+++ SISETG/src/java/dominio/RegistroContacto.java
@@ -39,7 +39,7 @@ public class RegistroContacto {
         }
         CallableStatement cs;
         try{
-            String sql="{ call sp_registroContacto(?,?,?,?,?,?,?,?,?,?,?)}";
+            String sql="{ call CONTACTO_Add(?,?,?,?,?,?,?,?,?,?,?)}";
             cs = connection.prepareCall(sql);
                 cs.setInt(1, getInstitucion());
                 cs.setString(2, getNombres());
